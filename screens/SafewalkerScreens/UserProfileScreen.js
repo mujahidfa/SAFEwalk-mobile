d6623f6377c472ff8c29575b3b9f93a02ca1a7b6
Mujahid Anuar <mujahidfanuar@gmail.com>
2020-03-09 02:48:10 -0500

    add styles to profile screens

--- screens/SafewalkerScreens/UserProfileScreen.js
+++ screens/SafewalkerScreens/UserProfileScreen.js
@@ -1,10 +1,79 @@
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-elements";
+import { Linking } from "expo";
+
+import { Ionicons, EvilIcons, FontAwesome } from "@expo/vector-icons";
+
+import colors from "./../../constants/colors";
 
 export default function UserProfileScreen({ navigation }) {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [address, setAddress] = useState("728 State St");
+  const [postalCode, setPostalCode] = useState("53715");
+  const [city, setCity] = useState("Madison");
+
+  useEffect(() => {
+    // later, call from server for user information
+    // for now, just set default values
+    setFirstname("Mujahid");
+    setLastname("Anuar");
+    setPhoneNumber("6081234567");
+  }, []);
+
+  function handleCall() {
+    Linking.openURL("tel:+1" + phoneNumber);
+  }
+
+  function handleText() {
+    Linking.openURL("sms:+1" + phoneNumber);
+  }
+
+  function handleMaps() {
+    let daddr = encodeURIComponent(`${address} ${postalCode}, ${city}`);
+
+    Linking.openURL(`https://maps.google.com/?daddr=${daddr}`);
+  }
+
+  function handleCancellation() {}
+
   return (
     <View style={styles.container}>
-      <Text>User Profile Screen</Text>
+      <FontAwesome
+        name="user-circle"
+        size={100}
+        style={styles.profilePicture}
+      />
+      <Text style={styles.textName}>
+        {firstname} {lastname}
+      </Text>
+      <View style={styles.buttonContactContainer}>
+        <Button
+          icon={<Ionicons name="ios-call" size={60} color={colors.white} />}
+          buttonStyle={styles.buttonCall}
+          onPress={() => handleCall()}
+        />
+        <Button
+          icon={<Ionicons name="md-text" size={60} color={colors.white} />}
+          buttonStyle={styles.buttonText}
+          onPress={() => handleText()}
+        />
+      </View>
+
+      <Button
+        title="Navigate using Maps"
+        buttonStyle={styles.buttonCancel}
+        icon={<EvilIcons name="external-link" size={25} color={colors.white} />}
+        iconRight={true}
+        onPress={() => handleMaps()}
+      />
+      <Button
+        title="Cancel"
+        buttonStyle={styles.buttonCancel}
+        onPress={() => handleCancellation()}
+      />
     </View>
   );
 }
@@ -12,8 +81,41 @@ export default function UserProfileScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: colors.white,
+    alignItems: "stretch",
     justifyContent: "center"
+  },
+  profilePicture: {
+    alignSelf: "center",
+    marginBottom: 30
+  },
+  textName: {
+    alignSelf: "center",
+    fontSize: 30,
+    marginBottom: 40
+  },
+  buttonContactContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginBottom: 100
+  },
+  buttonCall: {
+    backgroundColor: colors.gray,
+    borderRadius: 15,
+    width: 80,
+    height: 80
+  },
+  buttonText: {
+    backgroundColor: colors.gray,
+    borderRadius: 15,
+    width: 80,
+    height: 80
+  },
+  buttonCancel: {
+    marginBottom: 40,
+    height: 60,
+    borderRadius: 50,
+    backgroundColor: colors.red,
+    marginHorizontal: 40
   }
 });
--- screens/UserScreens/SafewalkerProfileScreen.js
+++ screens/UserScreens/SafewalkerProfileScreen.js
@@ -1,10 +1,63 @@
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-elements";
+import { Linking } from "expo";
+
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
+
+import colors from "./../../constants/colors";
 
 export default function SafewalkerProfileScreen({ navigation }) {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  useEffect(() => {
+    // later, call from server for user information
+    // for now, just set default values
+    setFirstname("Mujahid");
+    setLastname("Anuar");
+    setPhoneNumber("6081234567");
+  }, []);
+
+  function handleCall() {
+    Linking.openURL("tel:+1" + phoneNumber);
+  }
+
+  function handleText() {
+    Linking.openURL("sms:+1" + phoneNumber);
+  }
+
+  function handleCancellation() {}
+
   return (
     <View style={styles.container}>
-      <Text>Safewalker Profile Screen</Text>
+      <FontAwesome
+        name="user-circle"
+        size={100}
+        style={styles.profilePicture}
+      />
+      <Text style={styles.textName}>
+        {firstname} {lastname}
+      </Text>
+      <View style={styles.buttonContactContainer}>
+        <Button
+          icon={<Ionicons name="ios-call" size={60} color={colors.white} />}
+          buttonStyle={styles.buttonCall}
+          onPress={() => handleCall()}
+        />
+        <Button
+          icon={<Ionicons name="md-text" size={60} color={colors.white} />}
+          buttonStyle={styles.buttonText}
+          onPress={() => handleText()}
+        />
+      </View>
+
+      <Button
+        title="Cancel"
+        buttonStyle={styles.buttonCancel}
+        onPress={() => handleCancellation()}
+      />
     </View>
   );
 }
@@ -12,8 +65,41 @@ export default function SafewalkerProfileScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: colors.white,
+    alignItems: "stretch",
     justifyContent: "center"
+  },
+  profilePicture: {
+    alignSelf: "center",
+    marginBottom: 30
+  },
+  textName: {
+    alignSelf: "center",
+    fontSize: 30,
+    marginBottom: 40
+  },
+  buttonContactContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginBottom: 100
+  },
+  buttonCall: {
+    backgroundColor: colors.gray,
+    borderRadius: 15,
+    width: 80,
+    height: 80
+  },
+  buttonText: {
+    backgroundColor: colors.gray,
+    borderRadius: 15,
+    width: 80,
+    height: 80
+  },
+  buttonCancel: {
+    marginBottom: 40,
+    height: 60,
+    borderRadius: 50,
+    backgroundColor: colors.red,
+    marginHorizontal: 40
   }
 });
