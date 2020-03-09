08157725e2003efe3456882d34c17a436aee9722
Mujahid Anuar <mujahidfanuar@gmail.com>
2020-03-08 22:55:24 -0500

    add forms handling and styling in login page

---
+++ constants/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+  // red: "#b52f28",
+  red: "#c5050c",
+  darkred: "#9b0000",
+  orange: "#ed7c3a",
+  white: "#fff"
+};
+
+export default colors;
--- screens/AuthScreens/LoginScreens/UserLoginScreen.js
+++ screens/AuthScreens/LoginScreens/UserLoginScreen.js
@@ -1,53 +1,186 @@
-import React, { useState, useContext } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import React, { useContext, useEffect } from "react";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Alert
+} from "react-native";
+import { Button, Image } from "react-native-elements";
+import { TextInput } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useForm } from "react-hook-form";
+
+import colors from "./../../../constants/colors";
 
 import { AuthContext } from "./../../../contexts/AuthProvider";
 
 export default function UserLoginScreen({ navigation }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
   const { login } = useContext(AuthContext);
 
+  // forms input handling
+  const { register, setValue, handleSubmit, errors } = useForm();
+
+  // update email and password input upon change
+  useEffect(() => {
+    register("email");
+    register("password");
+  }, [register]);
+
+  // upon pressing the submit button
+  const onSubmit = data => {
+    const message =
+      "Email: " + data.email + "\n" + "Password: " + data.password;
+    Alert.alert("Form Data", message);
+    login("user", { email: data.email, password: data.password });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>User login Screen</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.logoUWContainer}>
+        <Image
+          source={require("./../../../assets/uw-transportation-logo.png")}
+          style={styles.logoUW}
+        />
+      </View>
 
-      <TextInput
-        placeholder="user@example.com"
-        value={email}
-        onChangeText={setEmail}
-      />
+      <KeyboardAvoidingView style={styles.innerContainer}>
+        <View style={styles.logoSAFEwalkContainer}>
+          <Text style={styles.logoSAFE}>SAFE</Text>
+          <Text style={styles.logoWALK}>walk</Text>
+        </View>
 
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-      />
+        {errors.email && (
+          <Text style={styles.textError}>wisc.edu email is required.</Text>
+        )}
+        <TextInput
+          label="Email"
+          placeholder="netid@wisc.edu"
+          ref={register(
+            { name: "email" },
+            { required: true, pattern: /^\S+@wisc\.edu$/i }
+          )}
+          onChangeText={text => setValue("email", text, true)}
+          mode="outlined"
+          theme={{ colors: { primary: colors.red } }}
+          style={styles.textInput}
+        />
 
-      <Button
-        title="Log in"
-        onPress={() => login("user", { email, password })}
-      />
+        {errors.password && (
+          <Text style={styles.textError}>Password is required.</Text>
+        )}
+        <TextInput
+          label="Password"
+          placeholder="Password"
+          ref={register({ name: "password" }, { required: true })}
+          onChangeText={text => setValue("password", text, true)}
+          mode="outlined"
+          secureTextEntry
+          theme={{ colors: { primary: colors.red } }}
+          style={styles.textInput}
+        />
 
-      <Button
-        title="Go to SAFEwalker login"
-        onPress={() => navigation.replace("SafewalkerLogin")}
-      />
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Login"
+            onPress={handleSubmit(onSubmit)}
+            // onPress={() => login("user", { email, password })}
+            buttonStyle={styles.buttonLogin}
+            titleStyle={styles.buttonLoginText}
+          />
 
-      <Button
-        title="Signup"
-        onPress={() => navigation.navigate("SignupStack")}
-      />
-    </View>
+          <Button
+            title="Don't have an account? Sign Up"
+            onPress={() => navigation.replace("SignupStack")}
+            buttonStyle={styles.buttonSignup}
+            titleStyle={styles.buttonSignupText}
+            containerStyle={styles.buttonSignupContainer}
+            type="outline"
+          />
+        </View>
+      </KeyboardAvoidingView>
+      <View style={styles.footerContainer}>
+        <Text style={styles.footerPrompt}>Login as a SAFEwalker?</Text>
+        <TouchableOpacity onPress={() => navigation.replace("SafewalkerLogin")}>
+          <Text style={styles.footerClickable}>Click here.</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: colors.white
+  },
+  logoUWContainer: {
+    alignItems: "center"
+  },
+  logoUW: {
+    width: 333,
+    height: 40,
+    alignSelf: "center"
+  },
+  innerContainer: {
+    flex: 1,
+    marginHorizontal: 50,
     justifyContent: "center"
+  },
+  logoSAFEwalkContainer: {
+    flexDirection: "row",
+    alignSelf: "center",
+    marginBottom: 50
+  },
+  logoSAFE: {
+    fontWeight: "bold",
+    fontSize: 75,
+    color: colors.orange
+  },
+  logoWALK: {
+    fontStyle: "italic",
+    fontSize: 75,
+    color: colors.orange
+  },
+  textError: {
+    color: colors.red
+  },
+  textInput: {
+    marginBottom: 20
+  },
+  buttonContainer: {
+    marginTop: 50
+  },
+  buttonLogin: {
+    marginBottom: 20,
+    height: 60,
+    borderRadius: 50,
+    backgroundColor: colors.red
+  },
+  buttonSignup: {
+    marginBottom: 20,
+    height: 60,
+    borderRadius: 50,
+    borderColor: colors.red
+  },
+  buttonLoginText: {
+    fontSize: 17
+  },
+  buttonSignupText: {
+    fontSize: 17,
+    color: colors.red
+  },
+  footerContainer: {
+    position: "absolute",
+    bottom: 0,
+    marginBottom: 30,
+    marginLeft: 50
+  },
+  footerPrompt: { fontSize: 20 },
+  footerClickable: {
+    fontSize: 20,
+    color: colors.darkred,
+    fontWeight: "bold"
   }
 });
