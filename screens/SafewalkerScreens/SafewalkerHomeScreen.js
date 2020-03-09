4c76054c81a469acfeddccbf661fee5e043eadb8
bajkowski <kbajkowski@wisc.edu>
2020-03-09 00:48:11 -0500

    Hardcoded in requests. Need to fetch data.

--- screens/SafewalkerScreens/SafewalkerHomeScreen.js
+++ screens/SafewalkerScreens/SafewalkerHomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext, Component} from "react";
-import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity, useState, Icon } from "react-native";
 import { ListItem, List} from 'react-native-elements';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
 
@@ -10,58 +10,15 @@ import { render } from "react-dom";
 export default function SafewalkerHomeScreen({ navigation }) {
   const { signout } = useContext(AuthContext);
 
-  const DATA = [
-    {
-      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-      title: 'Katie B',
-      name: 'kt',
-    },
-    {
-      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-      title: 'Alex D',
-      name: 'al',
-    },
-    {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: 'Yoon C',
-      name: 'yon',
-    },
-    {
-      id: '12',
-      title: 'test',
-      name: 'test',
-    },
-    {
-      id: '13',
-      title: 'test',
-      name: 'test',
-    },
-    {
-      id: '14',
-      title: 'test',
-      name: 'test',
-    },
-    {
-      id: '15',
-      title: 'test',
-      name: 'test',
-    },
-    {
-      id: '16',
-      title: 'test',
-      name: 'test',
-    },
-    {
-      id: '17',
-      title: 'test',
-      name: 'test',
-    },
-    {
-      id: '18',
-      title: 'test',
-      name: 'test',
-    }
-  ];
+  const [items, setItems] = React.useState([
+    {id: 1, name: 'Katie', time: '1:30 ', date: '3/14/20', start: 'start:KK ', end: 'end:UU'},
+    {id: 2, name: 'Alex', time: '1:17 ', date: '3/14/20', start: 'start:KK ', end: 'end:UU'},
+    {id: 3, name: 'Yoon', time: '1:15 ', date: '3/14/20', start: 'start:KK ', end: 'end:UU'},
+    {id: 4, name: 'Justin', time: '1:10 ', date: '3/14/20', start: 'start:KK ', end: 'end:UU'},
+    {id: 5, name: 'Mujahid', time: '1:05 ', date: '3/14/20', start: 'start:KK ', end: 'end:UU'},
+    {id: 6, name: 'Tadao', time: '1:00 ', date: '3/14/20', start: 'start:KK ', end: 'end:UU'},
+  ]);
+
 
   const LeftActions = () => {
     return (
@@ -75,46 +32,52 @@ export default function SafewalkerHomeScreen({ navigation }) {
     )
   };
 
-  const RightActions = (onPress) => {
+  const RightActions = ({onPress, deleteItem, item}) => {
     return (
       <View style={styles.RightAction}>
       <TouchableOpacity 
-      onPress={() => alert('pressed right!')}
+        onPress={() => alert('Request Denied')}
       >
         <Text style={styles.actionText}>Deny Request</Text>
       </TouchableOpacity>
       </View>
     )
   };
-  
-  function Item({ title }) {
+
+
+  const deleteItem = id => {
+    setItems((prevItems) => {
+      return prevItems.filter(item => item.id != id);
+    });
+  };
+
+  function Item({item, onPress, deleteItem}) {
     return (
-      <View style={styles.item}>
-        <Text style={styles.title}>{title}</Text>
-      </View>
+      <Swipeable
+      renderLeftActions={LeftActions}
+      renderRightActions={RightActions}
+    >
+    <View style={styles.row}>
+      <Text style={styles.name}>{item.name}</Text>
+      <Text style={styles.location}>{item.start}{item.end}</Text>
+      <Text style={styles.time}>{item.time}{item.date}</Text>
+      <Button
+      title = "Delete"
+      onPress={() => deleteItem(item.id)}
+      />
+    </View>
+    </Swipeable>
     );
   }
 
-  const onRightPress = () => {
-    alert('pressed right!')
-  }
 
   return (
     <View style={styles.container}>
       <FlatList
-        data={DATA}
+        data={items}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <Swipeable
-            renderLeftActions={LeftActions}
-            renderRightActions={RightActions}
-            //renderRightActions={() => <RightActions onPress={onRightPress}/>}
-          >
-          <View style={styles.row}>
-            <Text style={styles.name}>{item.title}</Text>
-            <Text style={styles.location}>{item.name}</Text>
-          </View>
-          </Swipeable>
+          <Item item={item} deleteItem={deleteItem} />
         )}
       />
     </View>
@@ -135,23 +98,22 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     flexDirection: 'row',
     borderColor: '#a9a9a9',
-    borderBottomWidth: 1,
-    backgroundColor: '#fff'
-  },
-  title: {
-    fontSize: 32,
+    borderBottomWidth: 2,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
   },
   name: {
-    fontSize: 30,
-    flex: 1,
-    justifyContent: 'flex-start',
+    fontSize: 25,
+    flex: 2,
+    fontWeight: '600',
   },
   location: {
     fontSize: 20,
-    justifyContent: 'flex-end',
+    flex: 2,
   },
   time: {
     fontSize: 20,
+    flex: 2,
   },
   LeftAction: {
     backgroundColor: '#388e3c',
