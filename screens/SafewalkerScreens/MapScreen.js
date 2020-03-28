b9370a508da535f82af5cdc7b74ffea797b957b2
Mujahid Anuar <mujahidfanuar@gmail.com>
2020-03-27 22:07:11 -0500

    add finish walk button

--- screens/SafewalkerScreens/MapScreen.js
+++ screens/SafewalkerScreens/MapScreen.js
@@ -1,10 +1,30 @@
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import { Button } from "react-native-elements";
+
+import colors from "./../../constants/colors";
 
 export default function MapScreen({ navigation }) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleSubmit() {
+    console.log("finish button is pressed!");
+  }
+
   return (
     <View style={styles.container}>
       <Text>SAFEwalker Map Screen</Text>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Mark as completed!"
+          loading={isLoading}
+          disabled={isLoading}
+          onPress={handleSubmit()}
+          buttonStyle={styles.buttonLogin}
+          titleStyle={styles.buttonLoginText}
+        />
+      </View>
     </View>
   );
 }
@@ -13,7 +33,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    alignItems: "stretch",
+    justifyContent: "center",
+    paddingHorizontal: 60
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    marginBottom: 40
+  },
+  buttonLogin: {
+    height: 60,
+    borderRadius: 50,
+    backgroundColor: colors.red
+    // position: "absolute",
+    // bottom: 0
+  },
+  buttonLoginText: {
+    fontSize: 20
   }
 });
