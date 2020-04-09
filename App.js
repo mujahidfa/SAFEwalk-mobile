690c71f5bc7e34b64c12e428d6674a876ee3bc5d
Mujahid Anuar <mujahidfanuar@gmail.com>
2020-04-09 17:57:13 -0500

    handle error boundary

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { View, Text, Button } from "react-native";
 import Main from "./Main";
 import { AuthProvider } from "./contexts/AuthProvider";
+import ErrorBoundary from "react-native-error-boundary";
+
+const CustomFallback = (props) => (
+  <View>
+    <Text>Something happened!</Text>
+    <Text>{props.error.toString()}</Text>
+    <Button onPress={props.resetError} title={"Try again"} />
+  </View>
+);
 
 export default function App() {
   return (
-    <AuthProvider>
-      <Main />
-    </AuthProvider>
+    <ErrorBoundary FallbackComponent={CustomFallback}>
+      <AuthProvider>
+        <Main />
+      </AuthProvider>
+    </ErrorBoundary>
   );
 }
