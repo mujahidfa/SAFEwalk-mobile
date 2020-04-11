93e21733f7d39fdfd688512de50a0afcd7de29ad
tshimura10 <43973122+tshimura10@users.noreply.github.com>
2020-04-11 02:33:56 -0500

    unstable update

--- screens/UserScreens/UserHomeScreen.js
+++ screens/UserScreens/UserHomeScreen.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
+import axios from 'axios';
 import {
   StyleSheet,
   Text,
@@ -16,7 +17,7 @@ import io from "socket.io-client";
 import colors from "./../../constants/colors";
 import socket from "./../../contexts/socket";
 import { AuthContext } from "./../../contexts/AuthProvider";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { Marker, PROVIDER_GOOGLE, fitToSuppliedMarkers } from "react-native-maps";
 
 const { width, height } = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
@@ -32,14 +33,52 @@ var initialRegion = {
   longitudeDelta: LONGITUDE_DELTA,
 };
 
-export default function UserHomeScreen({ navigation }) {
-  const [location, setLocation] = useState("");
-  const [destination, setDestination] = useState("");
-  const [time, setTime] = useState(new Date());
-  const [request, setRequest] = useState(false);
-  const [show, setShow] = useState(false);
-  const { userToken, email } = useContext(AuthContext);
+// temporary
+const homePlace = {
+  description: 'Home',
+  address: "",
+  coordinates: {
+    latitude: 43.081606,
+    longitude: -89.376298
+  }
+};
 
+export default function UserHomeScreen({ navigation }) {
+
+  // store current user location
+  const [location, setLocation] = useState({
+    coordinates: {
+      latitude: 43.081606,
+      longitude: -89.376298
+    },
+    address: ""
+  });
+
+  // destination
+  const [destination, setDestination] = useState({
+    coordinates: {
+      latitude: 43.081606,
+      longitude: -89.376298
+    },
+    address: ""
+  });
+
+  // walk origin
+  const [start, setStart] = useState({
+    coordinates: {
+      latitude: 43.081606,
+      longitude: -89.376298
+    },
+    address: ""
+  });
+
+  // text input values
+  const [startText, setStartText] = useState("");
+  const [endText, setEndText] = useState("");
+
+  const [eta, setEta] = useState("0");
+
+  // set window region of MapView
   const [region, setRegion] = useState(
     {
       region: {
@@ -50,6 +89,31 @@ export default function UserHomeScreen({ navigation }) {
       }
     }
   );
+
+  // markers and locations
+  const [markers, setMarkers] = useState([
+    {
+      key: 0,
+      title: 'Start',
+      coordinates: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE
+      }
+    },
+    {
+      key: 1,
+      title: 'Destination',
+      coordinates: {
+        // replace with api to get user's home addre
+        latitude: homePlace.coordinates.latitude,
+        longitude: homePlace.coordinates.longitude
+      }
+    }
+  ]);
+
+  const [request, setRequest] = useState(false);
+  const [show, setShow] = useState(false);
+  const { userToken, email } = useContext(AuthContext);
 
   async function getInitialState() {
     return {
@@ -72,6 +136,71 @@ export default function UserHomeScreen({ navigation }) {
 
   async function onRegionChange(region) {
     useState({ region });
+  }
+
+  // async function showStartEnd() {
+  //
+  //   setDestination({
+  //     coordinates: {
+  //       latitude: markers.coordinates.latitude,
+  //       longitude: markers.coordinates.longitude
+  //     }
+  //   })
+  //
+  //   setStart({
+  //     coordinates: {
+  //       latitude: markers[0].coordinates.latitude,
+  //       longitude: markers[0].coordinates.longitude
+  //     }
+  //   })
+  //
+  // }
+
+  async function getEta() {
+
+    // test:     https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=43.076492,-89.401185&destinations=44.076492,-89.401185&key=AIzaSyAOjTjRyHvY82Iw_TWRVGZl-VljNhRYZ-c
+    var axiosURL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + start.coordinates.latitude + ", " + start.coordinates.longitude + "&destinations=" + destination.coordinates.latitude + ", " + destination.coordinates.longitude + "&key=AIzaSyAOjTjRyHvY82Iw_TWRVGZl-VljNhRYZ-c";
+    axios.get(axiosURL)
+    .then(res => {
+      console.log("OUTPUT: " + res.data.rows[0].elements[0].duration.text);
+      setEta(res.data.rows[0].elements[0].duration.text);
+    })
+  }
+
+  const geocodeThis = coordinates => {
+    var axiosURL = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + coordinates.latitude + ", " + coordinates.longitude + "&key=AIzaSyAOjTjRyHvY82Iw_TWRVGZl-VljNhRYZ-c";
+    axios.get(axiosURL)
+    .then(res => {
+      console.log("OUTPUT: " + res.data.results[0].formatted_address);
+      return(res.data);
+    })
+  }
+
+  const updateMarker = index => e => {
+    console.log('index: ' + index);
+    console.log('property name: '+ e.nativeEvent.coordinate.latitude);
+    setMarkers({
+      coordinates: {
+        latitude: e.nativeEvent.coordinate.latitude,
+        longitude: e.nativeEvent.coordinate.longitude
+      }
+    })
+    geocodeThis(markers[index].coordinates);
+    if(index==0) {
+      setStart({
+        coordinates: {
+          latitude: e.nativeEvent.coordinate.latitude,
+          longitude: e.nativeEvent.coordinate.longitude
+        }
+      })
+    } else {
+      setDestination({
+        coordinates: {
+          latitude: e.nativeEvent.coordinate.latitude,
+          longitude: e.nativeEvent.coordinate.longitude
+        }
+      })
+    }
   }
 
   async function setSocketId() {
@@ -222,8 +351,6 @@ export default function UserHomeScreen({ navigation }) {
             initialRegion={initialRegion}
             provider={PROVIDER_GOOGLE}
             style={styles.mapStyle}
-            onRegionChangeComplete={onRegionChangeComplete}
-            onRegionChange={onRegionChange}
             showsUserLocation={true}
             ref={map => {
                 map = map;
@@ -234,9 +361,10 @@ export default function UserHomeScreen({ navigation }) {
             <Input
               inputStyle={styles.input}
               inputContainerStyle={styles.inputContainerTop}
-              placeholder="Start Location"
               value={location}
-              onChangeText={setLocation}
+              onChangeText={setStart}
+              placeholder='Start'
+              returnKeyType='search'
               leftIcon={{
                 type: "font-awesome",
                 name: "map-marker"
@@ -245,15 +373,31 @@ export default function UserHomeScreen({ navigation }) {
             <Input
               inputStyle={styles.input}
               inputContainerStyle={styles.inputContainer}
-              placeholder="Destination"
               value={destination}
               onChangeText={setDestination}
+              placeholder='Destination'
+              returnKeyType='search'
               leftIcon={{
                 type: "font-awesome",
                 name: "map-marker"
               }}
             />
+            <Text>{markers[1].coordinates.latitude}</Text>
+            <Text>{markers[1].coordinates.longitude}</Text>
+            <Text>{destination.address}</Text>
+            <Text>{eta}</Text>
+            {markers.map((marker, index) => (
+              <MapView.Marker
+                draggable
+                coordinate={marker.coordinates}
+                title={marker.title}
+                onDragEnd={updateMarker(index)}
+              />
+            ))}
           </MapView>
+          <TouchableOpacity onPress={() => getEta()}>
+            <Text style={styles.buttonConfirm}> ETA </Text>
+          </TouchableOpacity>
           <TouchableOpacity onPress={() => addRequest()}>
             <Text style={styles.buttonRequest}> Request SAFEwalk </Text>
           </TouchableOpacity>
@@ -296,7 +440,7 @@ const styles = StyleSheet.create({
   },
   buttonRequest: {
     backgroundColor: "#77b01a",
-    borderColor: colors.white,
+    borderColor: 'transparent',
     borderWidth: 1,
     borderRadius: 25,
     color: colors.white,
@@ -306,6 +450,19 @@ const styles = StyleSheet.create({
     padding: 12,
     textAlign: "center",
     marginBottom: 110
+  },
+  buttonConfirm: {
+    backgroundColor: "#77b01a",
+    borderColor: 'transparent',
+    borderWidth: 1,
+    borderRadius: 25,
+    color: colors.white,
+    fontSize: 24,
+    fontWeight: "bold",
+    overflow: "hidden",
+    padding: 12,
+    textAlign: "center",
+    marginBottom: 160
   },
   buttonCancel: {
     backgroundColor: colors.red,
@@ -347,7 +504,7 @@ const styles = StyleSheet.create({
     borderWidth: 2
   },
   mapStyle: {
-    marginTop: 50,
+    marginTop: 70,
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height-90,
   }
