1e46ec665dc1a0c62c2ad27e2b762cd99990b032
Mujahid Anuar <mujahidfanuar@gmail.com>
2020-04-13 10:40:00 -0500

    style sign in screens

--- screens/AuthScreens/SignupScreens/CredentialsScreen.js
+++ screens/AuthScreens/SignupScreens/CredentialsScreen.js
@@ -1,19 +1,34 @@
 import React, { useState, useEffect } from "react";
 import {
+  KeyboardAvoidingView,
+  Platform,
   StyleSheet,
   Text,
   View,
-  TouchableOpacity,
-  KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
 } from "react-native";
-import { Button } from "react-native-elements";
-import { TextInput } from "react-native-paper";
-import { useForm } from "react-hook-form";
 
+// Libraries
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useForm } from "react-hook-form";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+
+// Custom components
+import ErrorText from "./../../../components/ErrorText";
+import TextInput from "./../../../components/TextInput";
+import Button from "./../../../components/Button";
+import Or from "../../../components/Or";
+import Footer from "../../../components/AuthScreenFooter";
+import Spacer from "../../../components/Spacer";
+
+// Constants
 import colors from "./../../../constants/colors";
 import url from "./../../../constants/api";
+import style from "./../../../constants/style";
 
 export default function CredentialsSignupScreen({ navigation }) {
   const [isLoginError, setIsLoginError] = useState(false);
@@ -73,128 +88,125 @@ export default function CredentialsSignupScreen({ navigation }) {
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <View style={styles.container}>
-        {/* Progress animation */}
-        <View style={styles.progressContainer}>
-          <View style={styles.progressCircleContainer}>
-            <View style={[styles.progressCircle, styles.progressCurrentCircle]}>
-              <Text style={styles.progressCircleText}>1</Text>
+      <SafeAreaView style={styles.container}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={styles.innerContainer}
+        >
+          {/* Progress animation */}
+          <View style={styles.progressContainer}>
+            <View style={styles.progressCircleContainer}>
+              <View
+                style={[styles.progressCircle, styles.progressCurrentCircle]}
+              >
+                <Text style={styles.progressCircleText}>1</Text>
+              </View>
+              <Text style={styles.progressDescription}>Credentials</Text>
             </View>
-            <Text style={styles.progressDescription}>Credentials</Text>
+
+            <View style={[styles.progressLine, { marginRight: 3 }]} />
+
+            <View style={styles.progressCircleContainer}>
+              <View style={styles.progressCircle}>
+                <Text style={styles.progressCircleText}>2</Text>
+              </View>
+              <Text style={styles.progressDescription}>Basic Info</Text>
+            </View>
+
+            <View
+              style={[styles.progressLine, { marginLeft: 5, marginRight: 10 }]}
+            />
+
+            <View style={styles.progressCircleContainer}>
+              <View style={styles.progressCircle}>
+                <Text style={styles.progressCircleText}>3</Text>
+              </View>
+              <Text style={styles.progressDescription}>Finish</Text>
+            </View>
           </View>
 
-          <View style={[styles.progressLine, { marginRight: 3 }]} />
-
-          <View style={styles.progressCircleContainer}>
-            <View style={styles.progressCircle}>
-              <Text style={styles.progressCircleText}>2</Text>
+          {/* Input fields */}
+          <View>
+            <View style={styles.inputContainer}>
+              {errors.email && (
+                <ErrorText>wisc.edu email is required.</ErrorText>
+              )}
+              {isLoginError && (
+                <ErrorText>There was an error. Please try again.</ErrorText>
+              )}
+              {isUserNotAvailable && (
+                <ErrorText>Email is taken. Use a different email.</ErrorText>
+              )}
+              <TextInput
+                label="Email"
+                placeholder="netid@wisc.edu"
+                ref={register(
+                  { name: "email" },
+                  { required: true, pattern: /^[A-Z0-9._%+-]+@wisc\.edu$/i }
+                )}
+                onChangeText={(text) => setValue("email", text, true)}
+                mode="outlined"
+                keyboardType="email-address"
+                autoCapitalize="none"
+              />
             </View>
-            <Text style={styles.progressDescription}>Basic Info</Text>
+
+            <View style={styles.inputContainer}>
+              {errors.password && <ErrorText>Password is required.</ErrorText>}
+              <TextInput
+                label="Password"
+                placeholder="Password"
+                ref={register({ name: "password" }, { required: true })}
+                onChangeText={(text) => setValue("password", text, true)}
+                mode="outlined"
+                secureTextEntry
+              />
+            </View>
+
+            <View style={styles.inputContainer}>
+              {errors.confirmPassword && (
+                <ErrorText>The passwords do not match.</ErrorText>
+              )}
+              <TextInput
+                label="Confirm password"
+                ref={register(
+                  { name: "confirmPassword" },
+                  {
+                    required: true,
+                    validate: (value) =>
+                      value === watch("password") ||
+                      "The passwords do not match.",
+                  }
+                )}
+                onChangeText={(text) => setValue("confirmPassword", text, true)}
+                mode="outlined"
+                secureTextEntry
+              />
+            </View>
           </View>
 
-          <View
-            style={[styles.progressLine, { marginLeft: 5, marginRight: 10 }]}
-          />
-
-          <View style={styles.progressCircleContainer}>
-            <View style={styles.progressCircle}>
-              <Text style={styles.progressCircleText}>3</Text>
-            </View>
-            <Text style={styles.progressDescription}>Finish</Text>
-          </View>
-        </View>
-
-        {/* Main view */}
-        <KeyboardAvoidingView style={styles.innerContainer}>
-          {errors.email && (
-            <Text style={styles.textError}>wisc.edu email is required.</Text>
-          )}
-          <TextInput
-            label="Email"
-            placeholder="netid@wisc.edu"
-            ref={register(
-              { name: "email" },
-              { required: true, pattern: /^[A-Z0-9._%+-]+@wisc\.edu$/i }
-            )}
-            onChangeText={(text) => setValue("email", text, true)}
-            mode="outlined"
-            theme={{ colors: { primary: colors.red } }}
-            style={styles.textInput}
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
-
-          {errors.password && (
-            <Text style={styles.textError}>Password is required.</Text>
-          )}
-          <TextInput
-            label="Password"
-            placeholder="Password"
-            ref={register({ name: "password" }, { required: true })}
-            onChangeText={(text) => setValue("password", text, true)}
-            mode="outlined"
-            secureTextEntry
-            theme={{ colors: { primary: colors.red } }}
-            style={styles.textInput}
-          />
-
-          {errors.confirmPassword && (
-            <Text style={styles.textError}>The passwords do not match.</Text>
-          )}
-          <TextInput
-            label="Confirm password"
-            ref={register(
-              { name: "confirmPassword" },
-              {
-                required: true,
-                validate: (value) =>
-                  value === watch("password") || "The passwords do not match.",
+          {/* Footer */}
+          <View style={styles.footerContainer}>
+            <Spacer />
+            <Button
+              title="Next"
+              loading={isLoading}
+              disabled={isLoading}
+              onPress={handleSubmit(onSubmit)}
+            />
+            <Spacer />
+            <Or />
+            <Spacer />
+            <Footer
+              type="register"
+              onPress={() =>
+                navigation.dangerouslyGetParent().replace("UserLogin")
               }
-            )}
-            onChangeText={(text) => setValue("confirmPassword", text, true)}
-            mode="outlined"
-            secureTextEntry
-            theme={{ colors: { primary: colors.red } }}
-            style={styles.textInput}
-          />
-
-          {isLoginError && (
-            <Text style={styles.textErrorAPICall}>
-              There was an error. Please try again.
-            </Text>
-          )}
-          {isUserNotAvailable && (
-            <Text style={styles.textErrorAPICall}>
-              Email is taken. Use a different email.
-            </Text>
-          )}
+            />
+            <Spacer />
+          </View>
         </KeyboardAvoidingView>
-
-        {/* Footer */}
-        <Button
-          title="Next"
-          loading={isLoading}
-          disabled={isLoading}
-          onPress={handleSubmit(onSubmit)}
-          buttonStyle={styles.buttonNext}
-          titleStyle={styles.buttonNextText}
-        />
-        <View style={styles.orContainer}>
-          <View style={styles.orLine} />
-          <Text style={styles.orText}>or</Text>
-          <View style={styles.orLine} />
-        </View>
-        <View style={styles.footerContainer}>
-          <Text style={styles.footerPrompt}>Already have an account? </Text>
-          <TouchableOpacity
-            onPress={() =>
-              navigation.dangerouslyGetParent().replace("UserLogin")
-            }
-          >
-            <Text style={styles.footerClickable}>Sign in.</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      </SafeAreaView>
     </TouchableWithoutFeedback>
   );
 }
@@ -208,7 +220,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    marginTop: 50,
   },
   progressCircleContainer: {
     alignItems: "center",
@@ -220,10 +231,10 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
     borderRadius: 50,
-    borderWidth: 1,
+    borderWidth: 3,
   },
   progressCurrentCircle: {
-    borderColor: colors.red,
+    borderColor: colors.darkorange,
   },
   progressCircleText: {
     // textAlign: "center"
@@ -239,54 +250,14 @@ const styles = StyleSheet.create({
   },
   innerContainer: {
     flex: 1,
-    marginHorizontal: 50,
-    justifyContent: "center",
+    marginHorizontal: style.marginContainerHorizontal,
+    justifyContent: "space-between",
   },
-  textError: {
-    color: colors.red,
-  },
-  textInput: {
-    marginBottom: 20,
-  },
-  textErrorAPICall: {
-    color: colors.red,
-    alignSelf: "center",
-    fontSize: 18,
-  },
-  buttonNext: {
-    marginHorizontal: 50,
-    marginBottom: 20,
-    height: 50,
-    backgroundColor: colors.red,
-  },
-  buttonNextText: {
-    fontSize: 17,
-  },
-  orContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 15,
-  },
-  orText: {
-    fontSize: 20,
-    color: colors.lightgray,
-    marginHorizontal: 20,
-  },
-  orLine: {
-    borderWidth: 1,
-    borderColor: colors.lightgray,
-    width: 100,
+  inputContainer: {
+    height: hp("9.5%"),
+    justifyContent: "flex-end",
   },
   footerContainer: {
-    marginBottom: 50,
-    flexDirection: "row",
-    justifyContent: "center",
-  },
-  footerPrompt: { fontSize: 20 },
-  footerClickable: {
-    fontSize: 20,
-    color: colors.darkred,
-    fontWeight: "bold",
+    // marginHorizontal: style.marginContainerHorizontal,
   },
 });
--- screens/AuthScreens/SignupScreens/PersonalInfoScreen.js
+++ screens/AuthScreens/SignupScreens/PersonalInfoScreen.js
@@ -1,20 +1,35 @@
 import React, { useState, useEffect } from "react";
 import {
   KeyboardAvoidingView,
+  Platform,
   StyleSheet,
   Text,
   View,
-  TouchableOpacity,
   TouchableWithoutFeedback,
   Keyboard,
+  TouchableOpacity,
 } from "react-native";
 
-import { Button } from "react-native-elements";
-import { TextInput } from "react-native-paper";
+// Libraries
+import { SafeAreaView } from "react-native-safe-area-context";
 import { useForm } from "react-hook-form";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
+// Custom components
+import ErrorText from "./../../../components/ErrorText";
+import TextInput from "./../../../components/TextInput";
+import Button from "./../../../components/Button";
+import Or from "../../../components/Or";
+import Footer from "../../../components/AuthScreenFooter";
+import Spacer from "../../../components/Spacer";
+
+// Constants
 import colors from "./../../../constants/colors";
 import url from "./../../../constants/api";
+import style from "./../../../constants/style";
 
 export default function PersonalInfoScreen({ navigation, route }) {
   const [email, setEmail] = useState("");
@@ -122,125 +137,123 @@ export default function PersonalInfoScreen({ navigation, route }) {
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <View style={styles.container}>
-        {/* Progress animation */}
-        <View style={styles.progressContainer}>
-          <View style={styles.progressCircleContainer}>
-            <View style={[styles.progressCircle, styles.progressCurrentCircle]}>
-              <Text style={styles.progressCircleText}>1</Text>
+      <SafeAreaView style={styles.container}>
+        <KeyboardAvoidingView
+          behavior={Platform.Os == "ios" ? "padding" : "height"}
+          style={styles.innerContainer}
+        >
+          {/* Progress animation */}
+          <View style={styles.progressContainer}>
+            <View style={styles.progressCircleContainer}>
+              <View
+                style={[styles.progressCircle, styles.progressCurrentCircle]}
+              >
+                <Text style={styles.progressCircleText}>1</Text>
+              </View>
+              <Text style={styles.progressDescription}>Credentials</Text>
             </View>
-            <Text style={styles.progressDescription}>Credentials</Text>
+
+            <View
+              style={[
+                styles.progressLine,
+                styles.progressCurrentCircle,
+                { marginRight: 3 },
+              ]}
+            />
+
+            <View style={styles.progressCircleContainer}>
+              <View
+                style={[styles.progressCircle, styles.progressCurrentCircle]}
+              >
+                <Text style={styles.progressCircleText}>2</Text>
+              </View>
+              <Text style={styles.progressDescription}>Basic Info</Text>
+            </View>
+
+            <View
+              style={[styles.progressLine, { marginLeft: 5, marginRight: 10 }]}
+            />
+
+            <View style={styles.progressCircleContainer}>
+              <View style={styles.progressCircle}>
+                <Text style={styles.progressCircleText}>3</Text>
+              </View>
+              <Text style={styles.progressDescription}>Finish</Text>
+            </View>
           </View>
 
-          <View
-            style={[
-              styles.progressLine,
-              styles.progressCurrentCircle,
-              { marginRight: 3 },
-            ]}
-          />
-
-          <View style={styles.progressCircleContainer}>
-            <View style={[styles.progressCircle, styles.progressCurrentCircle]}>
-              <Text style={styles.progressCircleText}>2</Text>
+          {/* Input fields */}
+          <View>
+            <View style={styles.inputContainer}>
+              {errors.firstName && (
+                <ErrorText>First name is required.</ErrorText>
+              )}
+              {isSignupError && (
+                <ErrorText>There was an error. Please try again.</ErrorText>
+              )}
+              <TextInput
+                label="First Name"
+                ref={register({ name: "firstName" }, { required: true })}
+                onChangeText={(text) => setValue("firstName", text, true)}
+                mode="outlined"
+              />
             </View>
-            <Text style={styles.progressDescription}>Basic Info</Text>
+
+            <View style={styles.inputContainer}>
+              {errors.lastName && <ErrorText>Last name is required.</ErrorText>}
+              <TextInput
+                label="Last Name"
+                ref={register({ name: "lastName" }, { required: true })}
+                onChangeText={(text) => setValue("lastName", text, true)}
+                mode="outlined"
+              />
+            </View>
+
+            <View style={styles.inputContainer}>
+              {errors.phoneNumber && (
+                <ErrorText>Valid US phone number is required.</ErrorText>
+              )}
+              <TextInput
+                label="Phone Number"
+                placeholder="(608) - 123 - 4567"
+                ref={register(
+                  { name: "phoneNumber" },
+                  {
+                    required: true,
+                    minLength: 10,
+                    maxLength: 10,
+                    pattern: /^[0-9]+$/,
+                  }
+                )}
+                onChangeText={(text) => setValue("phoneNumber", text, true)}
+                mode="outlined"
+                keyboardType="phone-pad"
+              />
+            </View>
           </View>
 
-          <View
-            style={[styles.progressLine, { marginLeft: 5, marginRight: 10 }]}
-          />
-
-          <View style={styles.progressCircleContainer}>
-            <View style={styles.progressCircle}>
-              <Text style={styles.progressCircleText}>3</Text>
-            </View>
-            <Text style={styles.progressDescription}>Finish</Text>
-          </View>
-        </View>
-
-        {/* Main view */}
-        <KeyboardAvoidingView style={styles.innerContainer}>
-          {errors.firstName && (
-            <Text style={styles.textError}>First name is required.</Text>
-          )}
-          <TextInput
-            label="First Name"
-            ref={register({ name: "firstName" }, { required: true })}
-            onChangeText={(text) => setValue("firstName", text, true)}
-            mode="outlined"
-            theme={{ colors: { primary: colors.red } }}
-            style={styles.textInput}
-          />
-
-          {errors.lastName && (
-            <Text style={styles.textError}>Last name is required.</Text>
-          )}
-          <TextInput
-            label="Last Name"
-            ref={register({ name: "lastName" }, { required: true })}
-            onChangeText={(text) => setValue("lastName", text, true)}
-            mode="outlined"
-            theme={{ colors: { primary: colors.red } }}
-            style={styles.textInput}
-          />
-
-          {errors.phoneNumber && (
-            <Text style={styles.textError}>
-              Valid US phone number is required.
-            </Text>
-          )}
-          <TextInput
-            label="Phone Number"
-            placeholder="(608) - 123 - 4567"
-            ref={register(
-              { name: "phoneNumber" },
-              {
-                required: true,
-                minLength: 10,
-                maxLength: 10,
-                pattern: /^[0-9]+$/,
+          {/* Footer */}
+          <View style={styles.footerContainer}>
+            <Spacer />
+            <Button
+              title="Create Account"
+              loading={isLoading}
+              disabled={isLoading}
+              onPress={handleSubmit(onSubmit)}
+            />
+            <Spacer />
+            <Or />
+            <Spacer />
+            <Footer
+              type="register"
+              onPress={() =>
+                navigation.dangerouslyGetParent().replace("UserLogin")
               }
-            )}
-            onChangeText={(text) => setValue("phoneNumber", text, true)}
-            mode="outlined"
-            theme={{ colors: { primary: colors.red } }}
-            style={styles.textInput}
-            keyboardType="phone-pad"
-          />
-
-          {isSignupError && (
-            <Text style={styles.textErrorAPICall}>
-              There was an error. Please try again.
-            </Text>
-          )}
+            />
+            <Spacer />
+          </View>
         </KeyboardAvoidingView>
-
-        {/* Footer */}
-        <Button
-          title="Create Account"
-          loading={isLoading}
-          disabled={isLoading}
-          onPress={handleSubmit(onSubmit)}
-          buttonStyle={styles.buttonNext}
-          titleStyle={styles.buttonNextText}
-        />
-        <View style={styles.orContainer}>
-          <View style={styles.orLine} />
-          <Text style={styles.orText}>or</Text>
-          <View style={styles.orLine} />
-        </View>
-        <View style={styles.footerContainer}>
-          <Text style={styles.footerPrompt}>Already have an account? </Text>
-          <TouchableOpacity
-            onPress={() =>
-              navigation.dangerouslyGetParent().replace("UserLogin")
-            }
-          >
-            <Text style={styles.footerClickable}>Sign in.</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      </SafeAreaView>
     </TouchableWithoutFeedback>
   );
 }
@@ -254,7 +267,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    marginTop: 50,
   },
   progressCircleContainer: {
     alignItems: "center",
@@ -266,10 +278,10 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
     borderRadius: 50,
-    borderWidth: 1,
+    borderWidth: 3,
   },
   progressCurrentCircle: {
-    borderColor: colors.red,
+    borderColor: colors.darkorange,
   },
   progressCircleText: {
     // textAlign: "center"
@@ -285,54 +297,14 @@ const styles = StyleSheet.create({
   },
   innerContainer: {
     flex: 1,
-    marginHorizontal: 50,
-    justifyContent: "center",
+    marginHorizontal: style.marginContainerHorizontal,
+    justifyContent: "space-between",
   },
-  textError: {
-    color: colors.red,
-  },
-  textErrorAPICall: {
-    color: colors.red,
-    alignSelf: "center",
-    fontSize: 18,
-  },
-  textInput: {
-    marginBottom: 20,
-  },
-  buttonNext: {
-    marginHorizontal: 50,
-    marginBottom: 20,
-    height: 50,
-    backgroundColor: colors.red,
-  },
-  buttonNextText: {
-    fontSize: 17,
-  },
-  orContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 15,
-  },
-  orText: {
-    fontSize: 20,
-    color: colors.lightgray,
-    marginHorizontal: 20,
-  },
-  orLine: {
-    borderWidth: 1,
-    borderColor: colors.lightgray,
-    width: 100,
+  inputContainer: {
+    height: hp("9.5%"),
+    justifyContent: "flex-end",
   },
   footerContainer: {
-    marginBottom: 50,
-    flexDirection: "row",
-    justifyContent: "center",
-  },
-  footerPrompt: { fontSize: 20 },
-  footerClickable: {
-    fontSize: 20,
-    color: colors.darkred,
-    fontWeight: "bold",
+    // marginHorizontal: style.marginContainerHorizontal,
   },
 });
